2821e4886b5eec46e9c2fe2926a829ce363af6f9
Jarek <stachowski.jarek@wp.pl>
2019-03-26 17:33:30 +0100

    first commit

---
+++ src/main/java/pl/stachowski/WorkOrganizer/WorkOrganizerApplication.java
@@ -0,0 +1,13 @@
+package pl.stachowski.WorkOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/stachowski/WorkOrganizer/WorkOrganizerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.stachowski.WorkOrganizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkOrganizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
